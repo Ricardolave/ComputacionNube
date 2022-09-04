56d2f10a388473d9002dc78039786de65406960a
magotecnologia <marco@magotecnologia.com>
2022-09-03 21:06:52 -0500

    *Se agrega proyecto batch que realiza la correccion de malas palabras

---
+++ batch/src/main/java/edu/uanfilms/textcorrector/TextcorrectorApplication.java
@@ -0,0 +1,13 @@
+package edu.uanfilms.textcorrector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextcorrectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextcorrectorApplication.class, args);
+	}
+
+}
---
+++ batch/src/test/java/edu/uanfilms/textcorrector/TextcorrectorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uanfilms.textcorrector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextcorrectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
